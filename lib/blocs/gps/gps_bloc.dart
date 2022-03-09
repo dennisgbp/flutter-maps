076f64552647b34f2e67f5993a1fc7b01440a339
Dennis Calderón <developergbp48@gmail.com>
2022-03-09 09:19:06 -0500

    listener listo de cambio en servicio de GPS - video 188

---
+++ lib/blocs/gps/gps_bloc.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+
+part 'gps_event.dart';
+
+part 'gps_state.dart';
+
+class GpsBloc extends Bloc<GpsEvent, GpsState> {
+  GpsBloc()
+      : super(const GpsState(
+            isGpsEnabled: false, isGpsPermissionGranted: false)) {
+
+    on<GpsAndPermissionEvent>((event, emit) => emit(state.copyWith(
+        isGpsEnabled: event.isGpsEnabled,
+        isGpsPermissionGranted: event.isGpsPermissionGranted)));
+
+    _init();
+  }
+
+  Future<void> _init() async {
+    final isEnabled = await _checkGpsStatus();
+    print('isEnabled: $isEnabled');
+  }
+
+  Future<bool> _checkGpsStatus() async {
+    final isEnable = await Geolocator.isLocationServiceEnabled();
+
+    Geolocator.getServiceStatusStream().listen((event) {
+      final isEnable = (event.index == 1) ? true : false;
+      print('service status $isEnable');
+      //TODO: disparar eventos...
+
+    });
+    return isEnable;
+  }
+}
